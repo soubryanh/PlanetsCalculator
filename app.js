15447d33002b90493325cd5a14bdc277bbe97730
bry <bryanhernandz102@gmail.com>
2021-05-08 08:17:54 -0500

    git

---
+++ app.js
@@ -0,0 +1,26 @@
+
+var usuario = prompt("Cual es tu peso?");
+var planeta = parseInt(
+  prompt("Elige tu planeta\n1 es marte, 2 es jupiter")
+);
+var peso = parseInt(usuario);
+var g_tierra = 9.8;
+
+var g_marte = 3.7;
+var g_jupiter = 24.8;
+var peso_final;
+var nombre;
+if (planeta == 1) {
+  peso_final = (peso * g_marte) / g_tierra;
+  nombre = "Marte";
+} else if (planeta == 2) {
+  peso_final = (peso * g_jupiter) / g_tierra;
+  nombre = "Jupiter";
+} else {
+  peso_final = 1000000;
+  nombre = "Krypton";
+}
+peso_final = parseInt(peso_final);
+document.write(
+  "Tu peso en " + nombre + " es <strong>" + peso_final + " kilos</strong>"
+);
